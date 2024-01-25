cb7b9a2577f9a736e6c1facac8366e674429a066
NavyEOD_24 <68314394+NavyEOD@users.noreply.github.com>
2024-01-25 16:20:02 -0500

    Updated dialogue.js
    Added all dialogues with proper coordinates.

--- ABOMGame/dialogue.js
+++ ABOMGame/dialogue.js
@@ -1,3 +1,18 @@
 var dialogueObjects = {
+  // TEXT
+  "16,22": `What seems to be a highly damaged garage. The halting barriers and scanning devices to your right are highly damaged.`,
+  "66,26": `In the radiation lab, you can tell what eradicated life in this area. All forms of devices are clicking inside your suit.`,
+  "91,28": `The weapons bay is almost completely empty. Not that you needed ammunition, but did they have that many dangers to face?`,
+  "101,26": `"Please save us from this madness."`,
+  "115,21": `The entire side of the wall has been completely destroyed. Blood and rubble stain the ground. What caused this?`,
+  "32,17": `The elevator seems to be running on auxiliary power. A lever with a light above it is seen inside.`,
+  
+  // BODIES
+  "103,26": `A woman lies on the ground, her body cut 43 times. The entire top portion of her head was missing, eaten by a creature of some sorts.<br>Her mouth was stuck in an expression of pain.`,
+  "122,20": `A male security guard wearing a standard uniform would be stuck against the wall, rebar piercing his chest.<br>His left arm was still hanging onto the metal.`,
+  "125,24": `The body is unrecognizable, only an arm sticking out from underneath the concrete.`,
 
+  //UNKNOWNS
+  "128,21": `This creature was abhorrent to look at. The body didn't look like it could move at all.<br>Large, scythe-esque arms formed at the wrist and extended for meters.<br>It might even be able to pierce your armor.`,
+  "125,24": `The mass of flesh looked more like a clump of faces and bodies rather than a collective, composed being.<br>It writhed and still flinched when you walked close to it, and you saw a pair of eyes look at your visor.<br>Without hesitation, you exterminated it.`  
 }
