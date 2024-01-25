f8d6793fd63a691ad1644d7e93ed3b7cc92bd306
NavyEOD_24 <68314394+NavyEOD@users.noreply.github.com>
2024-01-25 18:09:19 -0500

    Updated gamelogic.js
    Swapped XY values for dialogue.

--- ABOMGame/gamelogic.js
+++ ABOMGame/gamelogic.js
@@ -130,28 +130,28 @@ var musicPlaying = false;
 
 var dialogueObjects = {
   // TEXT
-  "16,26": `What seems to be a highly damaged garage. The halting barriers and scanning devices to your right are highly damaged.`,
-  "66,30": `In the radiation lab, you can tell what eradicated life in this area. All forms of devices are clicking inside your suit.`,
-  "91,32": `The weapons bay is almost completely empty. Not that you needed ammunition, but did they have that many dangers to face?`,
-  "101,30": `"Please save us from this madness."`,
-  "115,25": `The entire side of the wall has been completely destroyed. Blood and rubble stain the ground. What caused this?`,
-  "32,22": `The elevator seems to be running on auxiliary power. A lever with a light above it is seen inside.`,
-  "45,23": `It's a standard Worldsoc humvee. Extra armor plating, better engine, and a better turret on top.<br>It seemed to be under maintenance.`,
-  "89,22": `Looked like a lab where samples of whatever needed to be tested on were put under the microscope.<br>It's wrecked.`,
-  "88,11": `A double-sided security gate. It was just wide enough for you to pass through.<br>Not that it would matter, with the entire facility being decommissioned.`,
-  "124,4": `The cafeteria was empty, left in the midst of a pizza Friday lunch.<br>It was rotting.`,
-  "133,20": `The remnants of a Leopard 2A6 main battle tank. The turret was decapitated and set back on the engine of the tank.<br>Whatever caused this might still be around.`,
-  "110,37": `The hallway stretches on for a while. Blood and corpses line the floor.`,
+  "26,16": `What seems to be a highly damaged garage. The halting barriers and scanning devices to your right are highly damaged.`,
+  "30,66": `In the radiation lab, you can tell what eradicated life in this area. All forms of devices are clicking inside your suit.`,
+  "32,91": `The weapons bay is almost completely empty. Not that you needed ammunition, but did they have that many dangers to face?`,
+  "30,101": `"Please save us from this madness."`,
+  "25,115": `The entire side of the wall has been completely destroyed. Blood and rubble stain the ground. What caused this?`,
+  "22,32": `The elevator seems to be running on auxiliary power. A lever with a light above it is seen inside.`,
+  "23,45": `It's a standard Worldsoc humvee. Extra armor plating, better engine, and a better turret on top.<br>It seemed to be under maintenance.`,
+  "22,89": `Looked like a lab where samples of whatever needed to be tested on were put under the microscope.<br>It's wrecked.`,
+  "11,88": `A double-sided security gate. It was just wide enough for you to pass through.<br>Not that it would matter, with the entire facility being decommissioned.`,
+  "4,124": `The cafeteria was empty, left in the midst of a pizza Friday lunch.<br>It was rotting.`,
+  "20,133": `The remnants of a Leopard 2A6 main battle tank. The turret was decapitated and set back on the engine of the tank.<br>Whatever caused this might still be around.`,
+  "37,110": `The hallway stretches on for a while. Blood and corpses line the floor.`,
   "30,36": `An enormous mass of flesh, bodies, and carcasses; a mass grave.`,
   
   // BODIES
-  "103,30": `A woman lies on the ground, her body cut 43 times. The entire top portion of her head was missing, eaten by a creature of some sorts.<br>Her mouth was stuck in an expression of pain.`,
-  "122,24": `A male security guard wearing a standard uniform would be stuck against the wall, rebar piercing his chest.<br>His left arm was still hanging onto the metal.`,
-  "125,28": `The body is unrecognizable, only an arm sticking out from underneath the concrete.`,
+  "30,103": `A woman lies on the ground, her body cut 43 times. The entire top portion of her head was missing, eaten by a creature of some sorts.<br>Her mouth was stuck in an expression of pain.`,
+  "24,122": `A male security guard wearing a standard uniform would be stuck against the wall, rebar piercing his chest.<br>His left arm was still hanging onto the metal.`,
+  "28,125": `The body is unrecognizable, only an arm sticking out from underneath the concrete.`,
 
   //UNKNOWNS
-  "128,25": `This creature was abhorrent to look at. The body didn't look like it could move at all.<br>Large, scythe-esque arms formed at the wrist and extended for meters.<br>It might even be able to pierce your armor.`,
-  "125,28": `The mass of flesh looked more like a clump of faces and bodies rather than a collective, composed being.<br>It writhed and still flinched when you walked close to it, and you saw a pair of eyes look at your visor.<br>Without hesitation, you exterminated it.`  
+  "25,128": `This creature was abhorrent to look at. The body didn't look like it could move at all.<br>Large, scythe-esque arms formed at the wrist and extended for meters.<br>It might even be able to pierce your armor.`,
+  "28,125": `The mass of flesh looked more like a clump of faces and bodies rather than a collective, composed being.<br>It writhed and still flinched when you walked close to it, and you saw a pair of eyes look at your visor.<br>Without hesitation, you exterminated it.`  
 }
 
 document.addEventListener("keydown", function(event) {
